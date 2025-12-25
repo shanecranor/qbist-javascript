7612c75390678aa5573d8948d834f3fff107e2c3
shanecranor <shanecranor@gmail.com>
2025-12-25 15:28:34 -0600

    clean up nits

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite"
 export default defineConfig({
   server: {
-    allowedHosts: ["qbist2.svgzip.com"], // 允许的域名
+    allowedHosts: ["qbist2.svgzip.com"],
   },
 })
